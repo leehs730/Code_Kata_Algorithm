409bb9cc1dcd7c2db0efabfb3a82714e4a41b168
leehs730 <75819612+leehs730@users.noreply.github.com>
2023-11-28 09:44:06 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.16 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.cs
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.cs
@@ -4,6 +4,7 @@ public class Solution {
     public long solution(long n) {
         long x = (long)Math.Sqrt(n);
         return (x*x == n) ? (x+1)*(x+1) : -1;
+        // 초기 실행 정답 코드, 제출 후 체점하면 반 이상 시간 초과로 탈락됨
         // for(int i = 1; i<n; i++)
         // {
         //     if(i*i == n){
